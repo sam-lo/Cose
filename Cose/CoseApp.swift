cd414c63934a4c14d50ab134ef4590b8a646a113
sam-lo <54739761+sam-lo@users.noreply.github.com>
2021-02-14 13:52:13 +0800

    Initial Commit

---
+++ Cose/CoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoseApp.swift
+//  Cose
+//
+//  Created by Mathilda on 14/2/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
